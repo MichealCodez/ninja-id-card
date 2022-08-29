3e3d98f319ebd3b4151c197952bdb01c0fa9b919
MADTech <madtechnow@gmail.com>
2022-08-29 12:49:46 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: NinjaID(),
+  ));
+}
+
+class NinjaID extends StatelessWidget {
+  const NinjaID({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[850 ],
+      appBar: AppBar(
+        title: const Text('Ninja ID Card'),
+        centerTitle: true,
+        backgroundColor: Colors.grey[800],
+        elevation: 0.0,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(30.0, 20.0, 30.0, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget> [
+            const Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/images/resized.JPG'),
+                radius: 80.0,
+              ),
+            ),
+            Divider(
+              height: 60.0,
+              color: Colors.grey[200],
+            ),
+            Text(
+              'NAME',
+              style: TextStyle(
+                color: Colors.grey[500],
+                letterSpacing: 2.0,
+              ),
+            ),
+            const SizedBox(height: 20.0,),
+            const Text(
+              'Micheal David',
+              style: TextStyle(
+                color: Colors.blueGrey,
+                letterSpacing: 2.0,
+                fontSize: 30.0,
+                fontWeight: FontWeight.bold
+              ),
+            ),
+            const SizedBox(height: 30.0,),
+            Text(
+              'NINJA LEVEL',
+              style: TextStyle(
+                color: Colors.grey[500],
+                letterSpacing: 2.0,
+              ),
+            ),
+            const SizedBox(height: 20.0,),
+            const Text(
+              '9',
+              style: TextStyle(
+                  color: Colors.blueGrey,
+                  letterSpacing: 2.0,
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold
+              ),
+            ),
+            const SizedBox(height: 30.0,),
+            Row(
+              children: <Widget> [
+                Icon(
+                  Icons.mail,
+                  color: Colors.grey[300],
+                  size: 30.0,
+                ),
+                 const SizedBox(width: 10.0,),
+                 Text(
+                   'madtechnow@gmail.com',
+                   style: TextStyle(
+                     color: Colors.grey[300],
+                     fontSize: 20.0,
+                     letterSpacing: 1.0,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
